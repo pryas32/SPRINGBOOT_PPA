fe3e7722291f90588593cb1d0cd9eca4b777e0fb
pryas32 <shuklaaayush805@gmail.com>
2025-05-11 02:55:02 +0530

    final commit

--- packaging/inhertiancetest.java
+++ packaging/inhertiancetest.java
@@ -7,7 +7,9 @@ public class inhertiancetest {
         inheritance i=new childlish(0, null, 0);//as paremnt is pointing to child samermethod which has significance in them too so no error
         i.abc();//this cant do as that method not decalred in parent
 
-
+//we can o like this also ;
+Object o=new childlish(0, null, 0);//we can point from object also 
+System.out.println(o.getClass());
 
 
 
