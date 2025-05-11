de2c46b819713a5d36cc8e01d982d5391884ff06
pryas32 <shuklaaayush805@gmail.com>
2025-05-12 02:32:00 +0530

    commit final

--- Exception/userclass.java
+++ Exception/userclass.java
@@ -1,26 +1,40 @@
 package Exception;
 
+import java.io.EOFException;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class userclass {
 
-public static void main(String args[])throws userdefinedexcpetion
+public static void main(String args[]) throws IOException
 {
+   try{
     calculation();
+   }
+   catch(Exception e)
+   {
+   //throw e;//here e is of IOException hence will not create problem
+  throw new IOException();   
+}
 }
 
-public static void calculation() throws userdefinedexcpetion
+public static void calculation() throws IOException,FileNotFoundException
 {
     try{
         FileReader file=new FileReader("C:\\Users\\LENOVO\\OneDrive\\Desktop\\dsu.txt");
     }
     catch(Exception e)
     {
-        throw new userdefinedexcpetion("message"+e);
+        throw new IOException();
     }
 }
 
 
+// we should handle the exception as late as possible we should always try to pass an exception.
+
+//in case of mismatch of exception exception get passed of callee.
+// we can decalre multiple exception within one block
 
 
 
