af58fbebd04cd62786ce842b39ddc1bb7ed210f8
pryas32 <shuklaaayush805@gmail.com>
2025-07-15 02:11:20 +0530

    commit

---
+++ revision/abc.java
@@ -0,0 +1,83 @@
+input devices-> the devices through which we give input to the computer are know as input devices
+eg ->mouse,keyword
+outpout devices-> the devices through which computer gives us an output eg printer
+scanner act as both input as well as output devices
+
+cpu->central process unit-used for compoutation purpose
+memory->used for storing the data
+cache-> used for frequrent usage of the data
+main memory->volatile-> ram-> our computer reads data from main menor ie ram
+secondary memory->rom this act as permamnent storage->non volatilr memory 
+why do we have three kinds of memory
+becuse kles qacces time 
+cap[acity and cost
+                                                                                                                                                                                                                                                                                         ]
+for a software to run its bought into the main memory from secondary storage and then executed by cpu step by step\
+operating system act as a maneger of the computer
+it help in interaction of the input & outpout devices and secondary storage with cpu and main memory
+cpu help in computation in  faster manner we need programming language to interact with cpu as cpu understand 0's and 1's
+programming languages like c++ ,java they have software like compiler and intrerpreter they help to convert the high level languages like C++,java into low lecvel langage which computer can understand
+compiler->it compiles the whole code into low level binary file.
+while interpter read the code line by line
+
+
+java is a platform independent .it was built in 1995.
+when we compile the java code it get converted into the bytecode which can run on any platform .That platform need to have jvm preinstalled on it
+for C/C++ they are not platform dependent because on compilation they generate executable format whcih is different for diffenet platform
+for running on different platform you need to have compiler of that platform installed in it.
+java has simple compilation and complex running code 
+while C++ has complex compilation as they need to generate machine specific binary code
+
+
+advantage of java over c++
+secure
+platform indepdenct
+pointers no use
+
+
+java is similiar to C++ means statically types means need to declare variable
+
+java has automatic garbage collection we need not to explicitdly deallocate dynamic memory
+
+//every code that we write in java has to be part of some class
+public class abc{statoc so that its object cant be created void retun type
+    main-> this is from where the execuition of the pgrm begins
+    public static void main(((
+
+
+    )String args[])//it takes array of string as an argument
+        so that it can be accesible out siode the class when jvm compiles it outide the pkg
+        public  
+    }
+    
+    
+    jvm-> java virtual machine->it is used for converting bytecode into machine specific binary code
+    jdk->it is an open source provided by oracle for java programming)
+
+jdk has jre and java class librares-> they are standard librares useed for running java pgrm
+jvm->used for converting bytecode into machine specific binary code
+dev tools-> these are the tools required for java programming eg java debugger
+javac test
+java test
+
+vairable and data type
+variable ->used for storing the value
+data type-> it tell who much memory memory is allocated to that vairable
+
+boolean-T| F
+1 byte->8 bit(-2^7 tio 2^7-1)
+short-> 2-> 16 but (-2^15 to 2^15-1)
+int->4 byte(-2^31 to 2^31-1)
+long->8 byte(-2^63 to 2^63-1)
+float and double
+char(0 to 2^16-1)
+
+we can have variable name no like this->0a ,1-9a,_a
+a!, a# ||a else if keyword cant be used as variale name 
+
+
+
+
+
+
+
