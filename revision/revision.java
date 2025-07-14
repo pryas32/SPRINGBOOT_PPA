990c2e30f6f628e4b2a0d2a709ab1da1a47df2f6
pryas32 <shuklaaayush805@gmail.com>
2025-07-15 01:01:39 +0530

    revision.java

---
+++ revision/revision.java
@@ -0,0 +1,39 @@
+input devices-> the devices through which we give input to the computer are know as input devices
+eg ->mouse,keyword
+outpout devices-> the devices through which computer gives us an output eg printer
+
+scanner act as both input as well as output devices
+
+cpu->central process unit-used for compoutation purpose
+memory->used for storing the data
+cache-> used for frequrent usage of the data
+main memory->volatile-> ram-> our computer reads data from main menor ie ram
+secondary memory->rom this act as permamnent storage->non volatilr memory 
+why do we have three kinds of memory
+becuse kles qacces time 
+cap[acity and cost
+                                                                                                                                                                                                                                                                                         ]
+for a software to run its bought into the main memory from secondary storage and then executed by cpu step by step\
+operating system act as a maneger of the computer
+it help in interaction of the input & outpout devices and secondary storage with cpu and main memory
+cpu help in computation in  faster manner we need programming language to interact with cpu as cpu understand 0's and 1's
+programming languages like c++ ,java they have software like compiler and intrerpreter they help to convert the high level languages like C++,java into low lecvel langage which computer can understand
+compiler->it compiles the whole code into low level binary file.
+while interpter read the code line by line
+
+
+java is a platform independent .it was built in 1995.
+when we compile the java code it get converted into the bytecode which can run on any platform .That platform need to have jvm preinstalled on it
+for C/C++ they are not platform dependent because on compilation they generate executable format whcih is different for diffenet platform
+for running on different platform you need to have compiler of that platform installed in it.
+java has simple compilation and complex running code 
+while C++ has complex compilation as they need to generate machine specific binary code
+
+
+advantage of java over c++
+secure
+platform indepdenct
+pointers no use
+
+
+java is similiar to C++ means statically types means need to declare variable
