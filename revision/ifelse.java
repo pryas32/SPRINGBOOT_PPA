24e032b3c56690beba795efae552f794759ca719
pryas32 <shuklaaayush805@gmail.com>
2025-07-16 01:58:36 +0530

    commit

---
+++ revision/ifelse.java
@@ -0,0 +1,28 @@
+public class ifelse{
+    public static void main(String args[])
+    {
+
+if(true)
+{
+    System.out.print("richoootsd");
+}
+else if(false)
+    {
+System.out.print("helllo world");
+    }
+else
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ revision/switching.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class switching{
+
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+
+String str=sc.nextLine();
+char ch=str.charAt(0);
+
+
+
+  
+
+switch (ch) {
+    case 'a':
+        System.out.println("Arijit");
+        break;
+    case 'b':
+    System.out.println("india");
+    break;
+
+    default:
+    System.out.print("hello world");
+}
+
+
+
+
+
+
+}
+
+
+    
+}
