48a4a79297ec0f467049f9b87a23558c16f5aa7a
pryas32 <shuklaaayush805@gmail.com>
2025-07-16 01:14:30 +0530

    commit

---
+++ revision/operators.java
@@ -0,0 +1,60 @@
+public class operators {
+    
+public static void main(String args[])
+{
+
+int x=12;
+int y=23;
+
+System.out.println(x*y);
+System.out.println(x-y);
+System.out.print(x/y);
+System.out.print(x%y);
+
+int x1=x++;
+++x;
+
+System.out.println(x1);
+System.out.println(x);
+
+//Assignment operators
+
+int c=12;
+int d=23;
+
+
+int z=y=x;
+
+
+here first x will be assigned then y and then z will be assigned
+
+arithemtic operator has from left to right while assigment operator has right to left
+
+(=,/=,%=,+=,*=,-=)
+
+logical && ,||,!)
+
+
+short circulating means checking one end and then not getting other
+
+F && next condition need not to check
+T || F
+T || =T
+T || F=T
+
+
+for bitwise
+
+                                                        
+a^a=0 , a^b=1;
+~
+
+
+
+
+
+
+}
+
+
+}
